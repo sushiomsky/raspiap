40dc456f58b6e1c3e2f8235936e9da64bdec98d8
sushiomsky <dennis.suchomsky@gmail.com>
2014-05-26 10:00:45 +0200

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -4,7 +4,7 @@ dpkg-reconfigure tzdata
 dpkg-reconfigure console-data
 dpkg-reconfigure locales
 apt-get -y install ca-certificates
-apt-get -y install wireless-tools
+#apt-get -y install wireless-tools
 apt-get -y install git-core curl
 #sudo curl -L --output /usr/bin/rpi-update https://raw.github.com/Hexxeh/rpi-update/master/rpi-update && sudo chmod +x /usr/bin/rpi-update
 #rpi-update
