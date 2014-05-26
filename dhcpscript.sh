e522221aefaa27606da66f6f5815a47ae71f8c1c
sushiomsky <dennis.suchomsky@gmail.com>
2014-05-26 04:23:26 +0200

    cleaning

--- dhcpscript.sh
+++ dhcpscript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #action when a new lease is added or removed
 echo  "Mac:" $2 "IP:" $3 $4 >> /var/log/leaselog.log
+/root/gpio_led.sh
 exit 0
---
+++ gpio_led.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+gpio  export 17 out
+gpio export 22 out
+gpio export 24 out
+
+gpio -g write 17 0
+gpio -g write 22 0
+gpio -g write 24 0
+
+gpio -g write 17 1
+sleep 1
+gpio -g write 22 1
+sleep 1
+gpio -g write 24 1
+sleep 1
+gpio -g write 17 0
+gpio -g write 22 0
+gpio -g write 24 0
+
+
+
+
+
+
+
+
