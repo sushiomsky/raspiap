aa2fb61ce2bb51a312db031ae9954a94788d2161
sushiomsky <dennis.suchomsky@gmail.com>
2014-05-30 18:54:31 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-chmod +x *
-cp adblock_update.sh sslstrip_rotate.sh dhcpscript.sh macchanger.sh simple_forwarding.sh ftp_backup.sh /usr/local/bin/
-
-cp -f rc.local /etc/rc.local
-cp -f .profile /root/
-
-
